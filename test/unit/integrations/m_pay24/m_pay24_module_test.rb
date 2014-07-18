ee4732ee65e5894407dfc39d0afb3c50b63f8478
Oskar M. Grande <oskar.grande@gmail.com>
2014-07-18 10:20:59 +0200

    Revert "MPay24 unit tests"
    This reverts commit 427effb0dc49310b0e9fca61dc0ee99a3026905e.

--- test/unit/integrations/m_pay24/m_pay24_module_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class MPay24Test < Test::Unit::TestCase
-  include OffsitePayments::Integrations
-
-  def test_notification_method
-    assert_instance_of MPay24::Notification, MPay24.notification('name=cody')
-  end
-end
