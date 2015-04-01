4cf5e33fdea442756686d825753ec35c953d88ce
David Rice <me@davidjrice.co.uk>
2015-04-01 17:36:43 +0100

    Add test for realex test mode

--- test/unit/integrations/realex/realex_notification_test.rb
+++ test/unit/integrations/realex/realex_notification_test.rb
@@ -11,6 +11,7 @@ class RealexNotificationTest < Test::Unit::TestCase
     assert @notification.verified?
     assert @notification.complete?
     assert @notification.acknowledge
+    assert !@notification.test?
 
     assert_equal 'ORD453-11', @notification.item_id
     assert_equal '13649024563820', @notification.transaction_id
@@ -23,10 +24,19 @@ class RealexNotificationTest < Test::Unit::TestCase
     assert_equal Money.new(5000, 'USD'), @notification.amount
   end
 
+  def test_test_mode
+    @notification = Realex::Notification.new(http_raw_data_test_mode, :credential3 => 'shared-secret')
+    assert @notification.test?
+  end
+
   private
 
   def http_raw_data
     "MERCHANT_ID=thestore&ACCOUNT=internet&ORDER_ID=ORD453-11&TIMESTAMP=20130814122239&AMOUNT=5000&X-CURRENCY=USD&SHA1HASH=dbd337bb359fa40312a7f7140ef72139cec7dace&RESULT=00&AUTHCODE=123420&MESSAGE=AUTHCODE=123420&PASREF=13649024563820&AVSPOSTCODERESULT=U&AVSADDRESSRESULT=U&CVNRESULT=M&BATCHID=870"
+  end
+
+  def http_raw_data_test_mode
+    "MERCHANT_ID=thestore&ACCOUNT=internet&ORDER_ID=ORD453-11&TIMESTAMP=20130814122239&AMOUNT=5000&X-CURRENCY=USD&SHA1HASH=dbd337bb359fa40312a7f7140ef72139cec7dace&RESULT=00&AUTHCODE=123420&MESSAGE=AUTHCODE=123420&PASREF=13649024563820&AVSPOSTCODERESULT=U&AVSADDRESSRESULT=U&CVNRESULT=M&BATCHID=870&X-TEST=true"
   end
 
 end
