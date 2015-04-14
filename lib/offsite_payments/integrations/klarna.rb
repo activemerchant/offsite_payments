5f2b6325cd164213c0004e01ee82b2bf558b5b35
Justin Mutter <mutter.justin@gmail.com>
2015-04-14 19:04:16 +0000

    Update Klarna integration to sign fewer fields and include checkout_token

--- lib/offsite_payments/integrations/klarna.rb
+++ lib/offsite_payments/integrations/klarna.rb
@@ -4,6 +4,8 @@ module OffsitePayments #:nodoc:
       mattr_accessor :service_url
       self.service_url = 'https://hpp-test.herokuapp.com/shopify/payment'
 
+      REQUIRED_FIELDS = %w(amount checkout_token merchant_base_uri merchant_checkout_uri merchant_confirmation_uri merchant_id merchant_terms_uri purchase_currency)
+
       def self.notification(post_body, options = {})
         Notification.new(post_body, options)
       end
@@ -15,23 +17,10 @@ module OffsitePayments #:nodoc:
       def self.cart_items_payload(fields, cart_items)
         check_required_fields!(fields)
 
-        payload = fields['purchase_country'].to_s +
-                  fields['purchase_currency'].to_s +
-                  fields['locale'].to_s
-
-        cart_items.each_with_index do |item, i|
-          payload << fields["cart_item-#{i}_type"].to_s +
-                     fields["cart_item-#{i}_reference"].to_s +
-                     fields["cart_item-#{i}_quantity"].to_s +
-                     fields["cart_item-#{i}_unit_price"].to_s +
-                     fields.fetch("cart_item-#{i}_discount_rate", '').to_s
+        payload = ""
+        REQUIRED_FIELDS.sort.each do |field|
+           payload << fields[field].to_s
         end
-
-        payload << fields['merchant_id'].to_s +
-                   fields['merchant_terms_uri'].to_s +
-                   fields['merchant_checkout_uri'].to_s +
-                   fields['merchant_base_uri'].to_s +
-                   fields['merchant_confirmation_uri'].to_s
 
         payload
       end
@@ -49,14 +38,7 @@ module OffsitePayments #:nodoc:
       private
 
       def self.check_required_fields!(fields)
-        %w(purchase_country
-           purchase_currency
-           locale
-           merchant_id
-           merchant_terms_uri
-           merchant_checkout_uri
-           merchant_base_uri
-           merchant_confirmation_uri).each do |required_field|
+        REQUIRED_FIELDS.each do |required_field|
           raise ArgumentError, "Missing required field #{required_field}" if fields[required_field].nil?
         end
       end
@@ -66,6 +48,8 @@ module OffsitePayments #:nodoc:
         mapping :cancel_return_url, ['merchant_terms_uri', 'merchant_checkout_uri', 'merchant_base_uri']
         mapping :account, 'merchant_id'
         mapping :customer, email: 'shipping_address_email'
+        mapping :checkout_token, 'checkout_token'
+        mapping :amount, 'amount'
 
         def initialize(order, account, options = {})
           super
