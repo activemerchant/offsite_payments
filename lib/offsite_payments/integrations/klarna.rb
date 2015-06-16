4ef8f9dae8c6d66ecfe3faf0d50f087903e1d51b
ivanfer <ivan.sanchez@shopify.com>
2015-06-16 17:47:24 +0000

    [Klarna] add path to url

--- lib/offsite_payments/integrations/klarna.rb
+++ lib/offsite_payments/integrations/klarna.rb
@@ -2,7 +2,7 @@ module OffsitePayments #:nodoc:
   module Integrations #:nodoc:
     module Klarna
       mattr_accessor :service_url
-      self.service_url = 'https://api.hostedcheckout.io'
+      self.service_url = 'https://api.hostedcheckout.io/shopify/payment'
 
       REQUIRED_FIELDS = %w(amount checkout_token merchant_base_uri merchant_checkout_uri merchant_confirmation_uri merchant_id merchant_terms_uri purchase_currency)
 
