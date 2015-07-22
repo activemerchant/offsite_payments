7d9509bd3aceb932507412ad1a690f24209a7120
andrewpaliga <andrew.paliga@shopify.com>
2015-07-21 23:10:57 -0400

    Revert "Switching depositflag to 0"

--- lib/offsite_payments/helper.rb
+++ lib/offsite_payments/helper.rb
@@ -16,7 +16,7 @@ module OffsitePayments #:nodoc:
     end
 
     def initialize(order, account, options = {})
-      options.assert_valid_keys([:amount, :currency, :test, :credential2, :credential3, :credential4, :country, :account_name, :description, :transaction_type, :authcode, :notify_url, :return_url, :redirect_param, :forward_url, :checkout_token, :deposit_flag])
+      options.assert_valid_keys([:amount, :currency, :test, :credential2, :credential3, :credential4, :country, :account_name, :description, :transaction_type, :authcode, :notify_url, :return_url, :redirect_param, :forward_url, :checkout_token])
       @fields             = {}
       @raw_html_fields    = []
       @test               = options[:test]
