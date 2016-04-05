4d3f356dfbe4aa8dba0a1f3efa2a88b7212345c8
Paytm-Payments <gitadmin@paytm.com>
2016-04-05 11:23:03 +0530

    checksum bug fixed

--- test/unit/integrations/paytm/paytm_module_test.rb
+++ test/unit/integrations/paytm/paytm_module_test.rb
@@ -117,8 +117,36 @@ class PaytmModuleTest < Test::Unit::TestCase
   end
 
   def test_checksum_method
-    paytm_load = { 'MID' => @merchant_id, 'ORDER_ID' => '100PT012' , 'CUST_ID' => 'test@example.com', 'TXN_AMOUNT'=> '10', 'CHANNEL_ID' => 'WEB' , 'INDUSTRY_TYPE_ID' => @industry_type_id, 'WEBSITE' => @website , 'EMAIL' => 'test@example.com', 'MOBILE_NO' => '9999999999'} 
-    checksum = new_pg_checksum(paytm_load, @secret_key)
+		paytm_load = { 'MID' => @merchant_id, 'ORDER_ID' => '100PT012' , 'CUST_ID' => 'test@example.com', 'TXN_AMOUNT'=> '10', 'CHANNEL_ID' => 'WEB' , 'INDUSTRY_TYPE_ID' => @industry_type_id, 'WEBSITE' => @website , 'EMAIL' => 'test@example.com', 'MOBILE_NO' => '9999999999'} 
+			
+			salt = '1234'
+			keys = paytm_load.keys
+			str = nil
+			keys = keys.sort
+			keys.each do |k|
+			  if str.nil?
+				str = paytm_load[k].to_s
+				next
+			  end
+			  str = str + '|'  + paytm_load[k].to_s
+			end
+			str = str + '|' + salt
+			
+			check_sum = Digest::SHA256.hexdigest(str)
+			check_sum = check_sum + salt
+			
+			### encrypting checksum ###
+			aes = OpenSSL::Cipher::Cipher.new("aes-128-cbc")
+			aes.encrypt
+			aes.key = secret_key
+			aes.iv = '@@@@&&&&####$$$$'
+			
+			encrypted_data = nil
+			encrypted_data = aes.update(check_sum.to_s) + aes.final
+			encrypted_data = Base64.encode64(encrypted_data)
+			
+			checksum = encrypted_data
+			
     assert_equal checksum, Paytm.checksum(paytm_load, @secret_key)
   end
 end
