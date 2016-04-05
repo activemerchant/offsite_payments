41530b591556691037ea70668b6fd4dd4802c803
Paytm-Payments <gitadmin@paytm.com>
2016-04-05 11:23:03 +0530

    checksum bug fixed

--- lib/offsite_payments/integrations/paytm.rb
+++ lib/offsite_payments/integrations/paytm.rb
@@ -6,7 +6,141 @@ require 'securerandom'
 module OffsitePayments #:nodoc:
   module Integrations #:nodoc:
     module Paytm
+<<<<<<< HEAD
       SALT_ALPHABET = ['a'..'z', 'A'..'Z', '0'..'9'].flat_map { |i| i.to_a }
+=======
+	
+	  require 'openssl'
+	  require 'base64'
+	  require 'digest'
+	  require 'securerandom'
+	
+	def new_pg_encrypt(params)
+    if (params.class != Hash) || (params.keys == [])
+      return false
+    end
+    if !params.has_key?(:key)
+      return false
+    end
+    encrypted_data = Hash[]
+    key = params.delete(:key)
+    keys = params.keys
+    aes = OpenSSL::Cipher::Cipher.new("aes-128-cbc")
+    begin
+      keys.each do |k|
+        data = params[k]
+        aes.encrypt
+        aes.key = key
+        aes.iv = '@@@@&&&&####$$$$'
+        encrypted_k = aes.update(k.to_s) + aes.final
+        encrypted_k = Base64.encode64(encrypted_k.to_s)
+        aes.encrypt
+        aes.key = key
+        aes.iv = '@@@@&&&&####$$$$'
+        encrypted_data[encrypted_k] = aes.update(data.to_s) + aes.final
+        encrypted_data[encrypted_k] = Base64.encode64(encrypted_data[encrypted_k])
+      end
+    rescue Exception => e
+      return false
+    end
+    return encrypted_data
+  end
+  
+  
+  def new_pg_encrypt_variable(data, key)
+    aes = OpenSSL::Cipher::Cipher.new("aes-128-cbc")
+    aes.encrypt
+    aes.key = key
+    aes.iv = '@@@@&&&&####$$$$'
+    encrypted_data = nil
+    begin
+      encrypted_data = aes.update(data.to_s) + aes.final
+      encrypted_data = Base64.encode64(encrypted_data)
+    rescue Exception => e
+      return false
+    end
+    return encrypted_data
+  end 
+  
+  
+  def new_pg_decrypt(params)
+    if (params.class != Hash) || (params.keys == [])
+      return false
+    end
+    if !params.has_key?(:key)
+      return false
+    end
+    decrypted_data = Hash[]
+    key = params.delete(:key)
+    keys = params.keys
+    aes = OpenSSL::Cipher::Cipher.new("aes-128-cbc")
+    begin
+      keys.each do |k|
+        data = params[k]
+        aes.decrypt
+        aes.key = key
+        aes.iv = '@@@@&&&&####$$$$'
+        decrypted_k = Base64.decode64(k.to_s)
+        decrypted_k = aes.update(decrypted_k.to_s) + aes.final
+        if data.empty?
+          decrypted_data[decrypted_k] = ""
+          next
+        end
+        aes.decrypt
+        aes.key = key
+        aes.iv = '@@@@&&&&####$$$$'
+        data = Base64.decode64(data)
+        decrypted_data[decrypted_k] = aes.update(data) + aes.final
+      end
+    rescue Exception => e
+      return false
+    end
+    return decrypted_data
+  end
+  
+  
+  def new_pg_decrypt_variable(data, key)
+    aes = OpenSSL::Cipher::Cipher.new("aes-128-cbc")
+    aes.decrypt
+    aes.key = key
+    aes.iv = '@@@@&&&&####$$$$'
+    decrypted_data = nil
+    begin
+      decrypted_data = Base64.decode64(data.to_s)
+      decrypted_data = aes.update(decrypted_data) + aes.final
+    rescue Exception => e
+      return false
+    end
+    return decrypted_data
+  end
+
+
+  def new_pg_generate_salt(length)
+    salt = SecureRandom.urlsafe_base64(length*(3.0/4.0))
+    return salt.to_s
+  end
+  
+  
+  def new_pg_checksum(params, key, salt_length = 4)
+    if params.class != Hash
+      return false
+    end
+    if key.empty?
+      return false
+    end
+    
+  end
+  
+  def new_pg_verify_checksum(params, check_sum, key, salt_length = 4)
+    
+    if params.class != Hash
+      return false
+    end
+
+    if key.empty?
+      return false
+    end
+>>>>>>> checksum bug fixed
 
       mattr_accessor :test_url
       mattr_accessor :production_url
@@ -26,6 +160,7 @@ module OffsitePayments #:nodoc:
         Return.new(post, options)
       end
 
+<<<<<<< HEAD
       def self.checksum(chk_params, secret_key, salt = nil)
         if salt.nil?
           salt = 4.times.map { SALT_ALPHABET[SecureRandom.random_number(SALT_ALPHABET.length)] }.join
@@ -60,6 +195,38 @@ module OffsitePayments #:nodoc:
         check_sum = Base64.strict_encode64(encrypted_data)
 
         check_sum
+=======
+      def self.checksum(params, secret_key )
+			#salt = SecureRandom.urlsafe_base64(4*(3.0/4.0))
+			salt = '1234'
+			keys = params.keys
+			str = nil
+			keys = keys.sort
+			keys.each do |k|
+			  if str.nil?
+				str = params[k].to_s
+				next
+			  end
+			  str = str + '|'  + params[k].to_s
+			end
+			str = str + '|' + salt
+			check_sum = Digest::SHA256.hexdigest(str)
+			check_sum = check_sum + salt
+			
+			### encrypting checksum ###
+			aes = OpenSSL::Cipher::Cipher.new("aes-128-cbc")
+			aes.encrypt
+			aes.key = secret_key
+			aes.iv = '@@@@&&&&####$$$$'
+			
+			encrypted_data = nil
+			encrypted_data = aes.update(check_sum.to_s) + aes.final
+			encrypted_data = Base64.encode64(encrypted_data)
+			
+			check_sum = encrypted_data
+			#new_pg_encrypt_variable(check_sum, key)
+			return check_sum
+>>>>>>> checksum bug fixed
       end
 
       class Helper < OffsitePayments::Helper
