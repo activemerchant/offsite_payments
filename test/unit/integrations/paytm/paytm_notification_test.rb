a9981602bc5407177385a46caca706d23e0e5677
Paytm-Payments <gitadmin@paytm.com>
2016-04-07 11:48:52 +0530

    issue fixed in test notification

--- test/unit/integrations/paytm/paytm_notification_test.rb
+++ test/unit/integrations/paytm/paytm_notification_test.rb
@@ -24,7 +24,7 @@ class PaytmNotificationTest < Test::Unit::TestCase
     assert_equal "9999999999", @paytm.customer_phone
     assert_equal "paytm-Admin", @paytm.customer_first_name
     assert_equal "", @paytm.customer_last_name
-    assert_equal "CeSZKawwkFriBwTYbGjwOSHQDNU2aR/69y0D+75aIOaN+1ivD/vn2gXiOpCR3XMSVIw0EZQG23X0x5dVt1OQwJEkvrUWLJ4MpprD3XL07KI=", @paytm.checksum
+    assert_equal "UgTNNLvjnFi/vxElGKstkBHJGbNCWDi+9pTnz5PhgpYefo89+HfI3fGdmkwhRCjLVKw/CIebMnER62PxVj2p2RDkZCOKXvr3JxOr75/AoLY=", @paytm.checksum
     #assert_equal "E000", @paytm.message
     assert_equal true, @paytm.checksum_ok?
   end
@@ -39,6 +39,7 @@ class PaytmNotificationTest < Test::Unit::TestCase
 
   private
   def http_raw_data
-   "MID=WorldP64425807474247&ORDER_ID=100PT012&CUST_ID=test@example.com&INDUSTRY_TYPE_ID=Retail&CHANNEL_ID=WEB&TXN_AMOUNT=10&WEBSITE=worldpressplg&MOBILE_NO=9999999999&EMAIL=test@example.com&CHECKSUMHASH=CeSZKawwkFriBwTYbGjwOSHQDNU2aR/69y0D+75aIOaN+1ivD/vn2gXiOpCR3XMSVIw0EZQG23X0x5dVt1OQwJEkvrUWLJ4MpprD3XL07KI="
+	"MID=WorldP64425807474247&ORDERID=100PT012&TXNAMOUNT=10&CURRENCY=INR&TXNID=494157&BANKTXNID=201512236592678&STATUS=TXN_SUCCESS&RESPCODE=01&RESPMSG=Txn+Successful&TXNDATE=2015-12-23+16:06:22.0&GATEWAYNAME=ICICI&BANKNAME=ICICI&PAYMENTMODE=DC&CHECKSUMHASH=UgTNNLvjnFi/vxElGKstkBHJGbNCWDi+9pTnz5PhgpYefo89+HfI3fGdmkwhRCjLVKw/CIebMnER62PxVj2p2RDkZCOKXvr3JxOr75/AoLY="
+   #"MID=WorldP64425807474247&ORDER_ID=100PT012&CUST_ID=test@example.com&INDUSTRY_TYPE_ID=Retail&CHANNEL_ID=WEB&TXN_AMOUNT=10&WEBSITE=worldpressplg&MOBILE_NO=9999999999&EMAIL=test@example.com&CHECKSUMHASH=CeSZKawwkFriBwTYbGjwOSHQDNU2aR/69y0D+75aIOaN+1ivD/vn2gXiOpCR3XMSVIw0EZQG23X0x5dVt1OQwJEkvrUWLJ4MpprD3XL07KI="
   end
 end
