988bcaaee0ed6f44d35253276126a52f48e3318e
Philip Tolton <mrtolton@gmail.com>
2016-06-22 16:28:01 -0400

    Inject loop not returning hash on guard clauses.

--- test/unit/return_test.rb
+++ test/unit/return_test.rb
@@ -7,4 +7,17 @@ class ReturnTest < Test::Unit::TestCase
     r = Return.new('')
     assert r.success?
   end
+
+  def test_parse
+    r = Return.new('')
+    assert r.parse('account=test').is_a?(Hash)
+  end
+
+  def test_parse_with_bad_query
+    r = Return.new('')
+    assert r.parse('&account=test').is_a?(Hash)
+    assert r.parse('key1=value1&key2&key3=value3').is_a?(Hash)
+    assert r.parse('foooo=value1&=value&key3=value3').is_a?(Hash)
+    assert r.parse('key1=value1&&key3=value3').is_a?(Hash)
+  end
 end
