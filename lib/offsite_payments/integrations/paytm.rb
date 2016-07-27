272324bba50d05f396acd55d4edf57b8f93f4482
Paytm-Payments <gitadmin@paytm.com>
2016-07-27 13:03:49 +0530

    commit styling on 27july2016

--- lib/offsite_payments/integrations/paytm.rb
+++ lib/offsite_payments/integrations/paytm.rb
@@ -1,15 +1,11 @@
 module OffsitePayments #:nodoc:
   module Integrations #:nodoc:
     module Paytm
-	
-		require 'openssl'
-		require 'base64'
-		require 'digest'
-		require 'securerandom'
-	
-		
-		
-	
+      require 'openssl'
+      require 'base64'
+      require 'digest'
+      require 'securerandom'
+
       mattr_accessor :test_url
       mattr_accessor :production_url
 
@@ -17,8 +13,8 @@ module OffsitePayments #:nodoc:
       self.production_url = 'https://secure.paytm.in/oltp-web/processTransaction'
 
       def self.service_url
-         #self.production_url
-		 OffsitePayments.mode == :production ? self.production_url : self.test_url
+        # self.production_url
+        OffsitePayments.mode == :production ? production_url : test_url
       end
 
       def self.notification(post, options = {})
@@ -29,81 +25,77 @@ module OffsitePayments #:nodoc:
         Return.new(post, options)
       end
 
-      def self.checksum(params, secret_key, salt = nil )
-		if salt.nil?
-			salt = SecureRandom.urlsafe_base64(4*(3/4))
-		end
-			keys = params.keys
-			str = nil
-			keys = keys.sort
-			keys.each do |k|
-			  if str.nil?
-				str = params[k].to_s
-				next
-			  end
-			  str = str + '|'  + params[k].to_s
-			end
-			str = str + '|' + salt
-			check_sum = Digest::SHA256.hexdigest(str)
-			check_sum = check_sum + salt
-			
-			### encrypting checksum ###
-			aes = OpenSSL::Cipher::Cipher.new("aes-128-cbc")
-			aes.encrypt
-			aes.key = secret_key
-			aes.iv = '@@@@&&&&####$$$$'
-			
-			encrypted_data = nil
-			encrypted_data = aes.update(check_sum.to_s) + aes.final
-			encrypted_data = Base64.encode64(encrypted_data)
-			
-			check_sum = encrypted_data.gsub("\n","")
-			#new_pg_encrypt_variable(check_sum, key)
-			return check_sum
+      def self.checksum(params, secret_key, salt = nil)
+        salt = SecureRandom.urlsafe_base64(4 * (3 / 4)) if salt.nil?
+        keys = params.keys
+        str = nil
+        keys = keys.sort
+        keys.each do |k|
+          if str.nil?
+            str = params[k].to_s
+            next
+          end
+          str = str + '|' + params[k].to_s
+        end
+        str = str + '|' + salt
+        check_sum = Digest::SHA256.hexdigest(str)
+        check_sum += salt
+
+        ### encrypting checksum ###
+        aes = OpenSSL::Cipher::Cipher.new('aes-128-cbc')
+        aes.encrypt
+        aes.key = secret_key
+        aes.iv = '@@@@&&&&####$$$$'
+
+        encrypted_data = nil
+        encrypted_data = aes.update(check_sum.to_s) + aes.final
+        encrypted_data = Base64.encode64(encrypted_data)
+
+        check_sum = encrypted_data.delete("\n")
+        # new_pg_encrypt_variable(check_sum, key)
+        check_sum
       end
 
       class Helper < OffsitePayments::Helper
-
-        CHECKSUM_FIELDS = [ 'MID', 'ORDER_ID', 'CUST_ID', 'TXN_AMOUNT', 'CHANNEL_ID', 'INDUSTRY_TYPE_ID', 'WEBSITE', 'EMAIL', 'MOBILE_NO']
+        CHECKSUM_FIELDS = %w(MID ORDER_ID CUST_ID TXN_AMOUNT CHANNEL_ID INDUSTRY_TYPE_ID WEBSITE EMAIL MOBILE_NO).freeze
 
         mapping :amount, 'TXN_AMOUNT'
         mapping :account, 'MID'
         mapping :order, 'ORDER_ID'
         mapping :description, 'productinfo'
 
-        mapping :customer, :first_name => 'firstname',
-          :last_name  => 'lastname',
-		  :email => 'CUST_ID',
-          :email => 'EMAIL',
-          :phone => 'MOBILE_NO'
+        mapping :customer, first_name: 'firstname',
+                           last_name: 'lastname',
+                           email: 'CUST_ID',
+                           email: 'EMAIL',
+                           phone: 'MOBILE_NO'
 
-        mapping :billing_address, :city => 'city',
-          :address1 => 'address1',
-          :address2 => 'address2',
-          :state => 'state',
-          :zip => 'zip',
-          :country => 'country'
+        mapping :billing_address, city: 'city',
+                                  address1: 'address1',
+                                  address2: 'address2',
+                                  state: 'state',
+                                  zip: 'zip',
+                                  country: 'country'
 
         # Which tab you want to be open default on Paytm
         # CC (CreditCard) or NB (NetBanking)
         mapping :mode, 'pg'
 
         mapping :notify_url, 'notify_url'
-        mapping :return_url, ['surl', 'furl']
+        mapping :return_url, %w(surl furl)
         mapping :cancel_return_url, 'curl'
         mapping :checksum, 'hash'
 
-        mapping :user_defined, { :var1 => 'udf1',
-          :var2 => 'udf2',
-          :var3 => 'udf3',
-          :var4 => 'udf4',
-          :var5 => 'udf5',
-          :var6 => 'udf6',
-          :var7 => 'udf7',
-          :var8 => 'udf8',
-          :var9 => 'udf9',
-          :var10 => 'udf10'
-        }
+        mapping :user_defined, var1: 'udf1',
+                               var2: 'udf2',
+                               var3: 'udf3',
+                               var4: 'udf4',
+                               var5: 'udf5',
+                               var6: 'udf6',
+                               var7: 'udf7',
+                               var8: 'udf8',
+                               var9: 'udf9',
+                               var10: 'udf10'
 
         def initialize(order, account, options = {})
           super
@@ -117,24 +109,22 @@ module OffsitePayments #:nodoc:
         end
 
         def generate_checksum
-		  @fields.merge(mappings[:CHANNEL_ID] => "WEB")
-		  @fields.merge(mappings[:INDUSTRY_TYPE_ID] => @options[:credential3])
-		  @fields.merge(mappings[:WEBSITE] => @options[:credential4])		  
+          @fields.merge(mappings[:CHANNEL_ID] => 'WEB')
+          @fields.merge(mappings[:INDUSTRY_TYPE_ID] => @options[:credential3])
+          @fields.merge(mappings[:WEBSITE] => @options[:credential4])
           checksum_payload_items = CHECKSUM_FIELDS.map { |field| @fields[field] }
-          Paytm.checksum(checksum_payload_items, @options[:credential2] )
+          Paytm.checksum(checksum_payload_items, @options[:credential2])
         end
 
         def sanitize_fields
-          ['address1', 'address2', 'city', 'state', 'country', 'productinfo', 'email', 'phone'].each do |field|
+          %w(address1 address2 city state country productinfo email phone).each do |field|
             @fields[field].gsub!(/[^a-zA-Z0-9\-_@\/\s.]/, '') if @fields[field]
           end
         end
-
       end
 
       class Notification < OffsitePayments::Notification
-	  
-		PaytmResponseParams = ['SUBS_ID','MID','BANKTXNID','TXNAMOUNT','CURRENCY','STATUS','RESPCODE','RESPMSG','TXNDATE','GATEWAYNAME','BANKNAME','PAYMENTMODE','PROMO_CAMP_ID','PROMO_STATUS','PROMO_RESPCODE','ORDERID','TXNID','REFUNDAMOUNT','REFID']
+        PaytmResponseParams = %w(SUBS_ID MID BANKTXNID TXNAMOUNT CURRENCY STATUS RESPCODE RESPMSG TXNDATE GATEWAYNAME BANKNAME PAYMENTMODE PROMO_CAMP_ID PROMO_STATUS PROMO_RESPCODE ORDERID TXNID REFUNDAMOUNT REFID).freeze
         def initialize(post, options = {})
           super(post, options)
           @merchant_id = options[:credential1]
@@ -142,7 +132,7 @@ module OffsitePayments #:nodoc:
         end
 
         def complete?
-          status == "Completed"
+          status == 'Completed'
         end
 
         def status
@@ -153,13 +143,13 @@ module OffsitePayments #:nodoc:
           end
         end
 
-        def invoice_ok?( order_id )
+        def invoice_ok?(order_id)
           order_id.to_s == invoice.to_s
         end
 
         # Order amount should be equal to gross - discount
-        def amount_ok?( order_amount, order_discount = BigDecimal.new( '0.0' ) )
-          BigDecimal.new( original_gross ) == order_amount
+        def amount_ok?(order_amount, _order_discount = BigDecimal.new('0.0'))
+          BigDecimal.new(original_gross) == order_amount
         end
 
         # Status of transaction return from the Paytm. List of possible values:
@@ -179,7 +169,7 @@ module OffsitePayments #:nodoc:
         #
         # 'CC' for credit-card
         # 'NB' for net-banking
-        
+
         def type
           params['PAYMENTMODE']
         end
@@ -212,77 +202,70 @@ module OffsitePayments #:nodoc:
           parse_and_round_gross_amount(params['TXNAMOUNT'])
         end
 
-               
-
         def checksum
-          params['CHECKSUMHASH'].gsub(" ","+")
+          params['CHECKSUMHASH'].tr(' ', '+')
         end
 
         def message
           @message || params['error']
         end
 
-        def acknowledge(authcode = nil)
+        def acknowledge(_authcode = nil)
           checksum_ok?
         end
 
         def checksum_ok?
-			
-			check_sum = checksum.gsub("\n","")
-			check_sum = check_sum.gsub(" ","+")
-			
-			if check_sum.nil?
-			  return false
-			end
-			
-			generated_check_sum = nil			
-			aes = OpenSSL::Cipher::Cipher.new("aes-128-cbc")
-			aes.decrypt
-			aes.key = @secret_key
-			aes.iv = '@@@@&&&&####$$$$'
-			decrypted_data = nil			
-		    decrypted_data = Base64.decode64(check_sum.to_s)
-			decrypted_data = aes.update(decrypted_data) + aes.final
-			check_sum  = decrypted_data
-			
-			if check_sum == false
-			  return false
-			end
-			
-			salt = check_sum[(check_sum.length-4), (check_sum.length)]
-			keys = params.keys
-			str = nil
-			keys = keys.sort
-			keys.each do |k|
-				if PaytmResponseParams.include?(k)                    
-					if str.nil?
-						 str = params[k].to_s
-						 next
-					end
-					str = str + '|' + params[k].to_s
-				end				
-			end
-			str = str + '|' + salt
-			generated_check_sum = Digest::SHA256.hexdigest(str)
-			generated_check_sum = generated_check_sum + salt			
-			if check_sum == generated_check_sum
-			    if params['RESPCODE'] == '01'
-					@message = params['RESPMSG']
-					return true
-				else
-					@message = 'Payment failed'
-					return false
-				end
-			else
-				@message = 'Return checksum not matching the data provided'
-				return false
-			end 
+          check_sum = checksum.delete("\n")
+          check_sum = check_sum.tr(' ', '+')
+
+          return false if check_sum.nil?
+
+          generated_check_sum = nil
+          aes = OpenSSL::Cipher::Cipher.new('aes-128-cbc')
+          aes.decrypt
+          aes.key = @secret_key
+          aes.iv = '@@@@&&&&####$$$$'
+          decrypted_data = nil
+          decrypted_data = Base64.decode64(check_sum.to_s)
+          decrypted_data = aes.update(decrypted_data) + aes.final
+          check_sum = decrypted_data
+
+          return false if check_sum == false
+
+          salt = check_sum[(check_sum.length - 4), check_sum.length]
+          keys = params.keys
+          str = nil
+          keys = keys.sort
+          keys.each do |k|
+            next unless PaytmResponseParams.include?(k)
+            if str.nil?
+              str = params[k].to_s
+              next
+            end
+            str = str + '|' + params[k].to_s
+          end
+          str = str + '|' + salt
+          generated_check_sum = Digest::SHA256.hexdigest(str)
+          generated_check_sum += salt
+          if check_sum == generated_check_sum
+            if params['RESPCODE'] == '01'
+              @message = params['RESPMSG']
+              return true
+            else
+              @message = 'Payment failed'
+              return false
+            end
+          else
+            @message = 'Return checksum not matching the data provided'
+            return false
+          end
         end
 
         private
+
         def parse_and_round_gross_amount(amount)
           rounded_amount = (amount.to_f * 100.0).round
-          sprintf("%.2f", rounded_amount / 100.00)
+          sprintf('%.2f', rounded_amount / 100.00)
         end
       end
 
@@ -293,11 +276,11 @@ module OffsitePayments #:nodoc:
         end
 
         def transaction_id
-          @notification.transaction_id 
+          @notification.transaction_id
         end
 
-        def status( order_id, order_amount )
-          if @notification.invoice_ok?( order_id ) && @notification.amount_ok?( BigDecimal.new(order_amount) )
+        def status(order_id, order_amount)
+          if @notification.invoice_ok?(order_id) && @notification.amount_ok?(BigDecimal.new(order_amount))
             @notification.status
           else
             'Mismatch'
@@ -305,7 +288,7 @@ module OffsitePayments #:nodoc:
         end
 
         def success?
-          status( @params['ORDERID'], @params['TXNAMOUNT'] ) == 'Completed'
+          status(@params['ORDERID'], @params['TXNAMOUNT']) == 'Completed'
         end
 
         def message
--- test/unit/integrations/paytm/paytm_module_test.rb
+++ test/unit/integrations/paytm/paytm_module_test.rb
@@ -3,9 +3,6 @@ require 'openssl'
 require 'base64'
 require 'digest'
 require 'securerandom'
-	
-	
-  
 
 class PaytmModuleTest < Test::Unit::TestCase
   include OffsitePayments::Integrations
@@ -14,16 +11,16 @@ class PaytmModuleTest < Test::Unit::TestCase
     OffsitePayments.mode = :test
     @merchant_id = 'WorldP64425807474247'
     @secret_key = 'kbzk1DSbJiV_O3p5'
-	@industry_type_id = 'Retail'
-	@website = 'worldpressplg'
+    @industry_type_id = 'Retail'
+    @website = 'worldpressplg'
   end
 
   def test_service_url_method
     OffsitePayments.mode = :test
-    assert_equal "https://pguat.paytm.com/oltp-web/processTransaction", Paytm.service_url
+    assert_equal 'https://pguat.paytm.com/oltp-web/processTransaction', Paytm.service_url
 
     OffsitePayments.mode = :production
-    assert_equal "https://secure.paytm.in/oltp-web/processTransaction", Paytm.service_url
+    assert_equal 'https://secure.paytm.in/oltp-web/processTransaction', Paytm.service_url
   ensure
     OffsitePayments.mode = :test
   end
@@ -37,36 +34,36 @@ class PaytmModuleTest < Test::Unit::TestCase
   end
 
   def test_checksum_method
-		paytm_load = { 'MID' => @merchant_id, 'ORDER_ID' => '100PT012' , 'CUST_ID' => 'test@example.com', 'TXN_AMOUNT'=> '10', 'CHANNEL_ID' => 'WEB' , 'INDUSTRY_TYPE_ID' => @industry_type_id, 'WEBSITE' => @website , 'EMAIL' => 'test@example.com', 'MOBILE_NO' => '9999999999'} 
-			
-			salt = '1234'
-			keys = paytm_load.keys
-			str = nil
-			keys = keys.sort
-			keys.each do |k|
-			  if str.nil?
-				str = paytm_load[k].to_s
-				next
-			  end
-			  str = str + '|'  + paytm_load[k].to_s
-			end
-			str = str + '|' + salt
-			
-			check_sum = Digest::SHA256.hexdigest(str)
-			check_sum = check_sum + salt
-			
-			### encrypting checksum ###
-			aes = OpenSSL::Cipher::Cipher.new("aes-128-cbc")
-			aes.encrypt
-			aes.key = @secret_key
-			aes.iv = '@@@@&&&&####$$$$'
-			
-			encrypted_data = nil
-			encrypted_data = aes.update(check_sum.to_s) + aes.final
-			encrypted_data = Base64.encode64(encrypted_data)
-			
-			checksum = encrypted_data.gsub("\n","")
-			
+    paytm_load = { 'MID' => @merchant_id, 'ORDER_ID' => '100PT012', 'CUST_ID' => 'test@example.com', 'TXN_AMOUNT' => '10', 'CHANNEL_ID' => 'WEB', 'INDUSTRY_TYPE_ID' => @industry_type_id, 'WEBSITE' => @website, 'EMAIL' => 'test@example.com', 'MOBILE_NO' => '9999999999' }
+
+    salt = '1234'
+    keys = paytm_load.keys
+    str = nil
+    keys = keys.sort
+    keys.each do |k|
+      if str.nil?
+        str = paytm_load[k].to_s
+        next
+      end
+      str = str + '|' + paytm_load[k].to_s
+    end
+    str = str + '|' + salt
+
+    check_sum = Digest::SHA256.hexdigest(str)
+    check_sum += salt
+
+    ### encrypting checksum ###
+    aes = OpenSSL::Cipher::Cipher.new('aes-128-cbc')
+    aes.encrypt
+    aes.key = @secret_key
+    aes.iv = '@@@@&&&&####$$$$'
+
+    encrypted_data = nil
+    encrypted_data = aes.update(check_sum.to_s) + aes.final
+    encrypted_data = Base64.encode64(encrypted_data)
+
+    checksum = encrypted_data.delete("\n")
+
     assert_equal checksum, Paytm.checksum(paytm_load, @secret_key, '1234')
   end
 end
