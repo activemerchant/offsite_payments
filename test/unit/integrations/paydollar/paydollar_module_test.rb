ba325410b0a64fa6bda6e81b04323a918070c114
SimonLeungAPHK <simon.leung@asiapay.com>
2017-03-31 14:30:50 +0800

    Update testing url to payShopify.jsp

--- test/unit/integrations/paydollar/paydollar_module_test.rb
+++ test/unit/integrations/paydollar/paydollar_module_test.rb
@@ -13,14 +13,14 @@ class PaydollarModuleTest < Test::Unit::TestCase
 
   def test_production_url
     OffsitePayments.mode = :production
-    assert_equal 'https://www.paydollar.com/b2c2/eng/payment/payForm.jsp', Paydollar.service_url
+    assert_equal 'https://www.paydollar.com/b2c2/eng/payment/payShopify.jsp', Paydollar.service_url
   ensure
     OffsitePayments.mode = :test
   end
 
   def test_test_url
     OffsitePayments.mode = :test
-    assert_equal 'https://test.paydollar.com/b2cDemo/eng/payment/payForm.jsp', Paydollar.service_url
+    assert_equal 'https://test.paydollar.com/b2cDemo/eng/payment/payShopify.jsp', Paydollar.service_url
   end
 
   def test_currency_map
