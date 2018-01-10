7b87516054b66fcff1f437daa8c5d5866d536c97
Ahamed Shah Wahidullah <23074455+ahamed-wahid@users.noreply.github.com>
2018-01-10 08:50:16 +0530

    re pushing to trigger the build

--- lib/offsite_payments/integrations/payu_in.rb
+++ lib/offsite_payments/integrations/payu_in.rb
@@ -70,7 +70,7 @@ module OffsitePayments #:nodoc:
           super
           @options = options
           self.pg = 'CC'
-          @fields['udf5'] = 'shopify_active_merchants' 
+          @fields['udf5'] = 'shopify_active_merchants'
         end
 
         def form_fields
