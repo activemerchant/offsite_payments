8dd54aa386503cbec18fee94985acda90f31bce5
Pierre Nespo <pierre.nespo@shopify.com>
2019-07-10 13:42:02 -0400

    Remove test case for v2 token as there is no difference between tokens

--- test/unit/integrations/bit_pay/bit_pay_notification_test.rb
+++ test/unit/integrations/bit_pay/bit_pay_notification_test.rb
@@ -4,10 +4,9 @@ class BitPayNotificationTest < Test::Unit::TestCase
   include OffsitePayments::Integrations
 
   def setup
-    @token_v1 = 'g82hEYhfRkhIlX5HJEqO8w5giRVeyGwsJ1wDPRvx8'
-    @token_v2 = '5v2K2rwuWQbnKexiQF9Eu6xdCVg7HFtkFNXarGEq9vLR'
+    @token = 'g82hEYhfRkhIlX5HJEqO8w5giRVeyGwsJ1wDPRvx8'
     @invoice_id = '98kui1gJ7FocK41gUaBZxG'
-    @bit_pay = BitPay::Notification.new(http_raw_data, credential1: @token_v1)
+    @bit_pay = BitPay::Notification.new(http_raw_data, credential1: @token)
   end
 
   def test_accessors
@@ -24,20 +23,11 @@ class BitPayNotificationTest < Test::Unit::TestCase
     assert_equal Money.from_amount(10.00, 'USD'), @bit_pay.amount
   end
 
-  def test_successful_acknowledgement_with_v1_api_token
+  def test_successful_acknowledgement
     stub_request(:get, "#{BitPay::API_V2_URL}/#{@invoice_id}")
       .to_return(status: 200, body: http_raw_data)
 
     assert @bit_pay.acknowledge
-  end
-
-  def test_successful_acknowledgement_with_v2_api_token
-    stub_request(:get, "#{BitPay::API_V2_URL}/#{@invoice_id}")
-      .to_return(status: 200, body: http_raw_data)
-
-    notification = BitPay::Notification.new(http_raw_data, credential1: @token_v2)
-
-    assert notification.acknowledge
   end
 
   def test_acknowledgement_error
