68f92587c30e6978ac5c054732404f3e086603fa
Daniel Lepage <daniel@daniellepage.com>
2022-01-27 09:34:52 -0500

    Update realex_offsite.rb
    Update production and staging api URLs. (Reference: https://developer.globalpay.com/api/getting-started )

--- lib/offsite_payments/integrations/realex_offsite.rb
+++ lib/offsite_payments/integrations/realex_offsite.rb
@@ -3,8 +3,8 @@ module OffsitePayments #:nodoc:
     module RealexOffsite
       mattr_accessor :production_url
       mattr_accessor :test_url
-      self.production_url = 'https://epage.payandshop.com/epage.cgi'
-      self.test_url       = 'https://hpp.sandbox.realexpayments.com/pay'
+      self.production_url = 'https://api.realexpayments.com/epage-remote.cgi'
+      self.test_url       = 'https://api.sandbox.realexpayments.com/epage-remote.cgi'
 
       def self.helper(order, account, options={})
         Helper.new(order, account, options)
